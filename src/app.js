1ffb08735e1129055942acd78cd99e7807e54c58
jude <jude@firstcoin.com>
2018-06-23 13:03:14 -0700

    remove unused code.

--- src/app.js
+++ src/app.js
@@ -27,13 +27,6 @@ scanner = new Instascan.Scanner({ video: document.getElementById('preview'), sca
 scanner.addListener('scan', function (content, image) {
   console.log('content', content);
   alert(content);
-  let win = new BrowserWindow({width: 800, height: 600})
-  win.on('closed', () => {
-    win = null
-  })
-  
-  // Load a remote URL
-  win.loadURL('https://github.com')
   // scans.unshift({ date: +(Date.now()), content: content });
 });
 console.log(Instascan);
@@ -58,27 +51,9 @@ navigator.mediaDevices.getUserMedia({video: true})
   });
 
 document.addEventListener('DOMContentLoaded', () => {
-  // document.querySelector('#record-desktop').addEventListener('click', recordDesktop)
   document.querySelector('#record-camera').addEventListener('click', recordCamera)
 })
 
 const recordCamera = () => {
-  cleanRecord()
-  navigator.webkitGetUserMedia({
-    audio: false,
-    video: { mandatory: { minWidth: 1280, minHeight: 720 } }
-  }, getMediaStream, getUserMediaError)
-}
-
-const onAccessApproved = (id) => {
-  if (!id) {
-    console.log('Access rejected.')
-    return
-  }
-  console.log('Window ID: ', id)
-  navigator.webkitGetUserMedia({
-    audio: false,
-    video: { mandatory: { chromeMediaSource: 'desktop', chromeMediaSourceId: id,
-      maxWidth: window.screen.width, maxHeight: window.screen.height } }
-  }, getMediaStream, getUserMediaError)
+  ipcRenderer.send('show-picker', { types: ['screen'] })
 }
